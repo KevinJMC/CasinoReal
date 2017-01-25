bc5c2c1637e6f5e7b9ebd28f0acf7663874da59a
Kevin McCann <kevinmccann@zipcoders-MacBook-Pro-5.local>
2017-01-25 15:37:07 -0500

    CardGame extends game

--- casino/src/main/java/com/casinoreal/CardGames.java
+++ casino/src/main/java/com/casinoreal/CardGames.java
@@ -3,7 +3,7 @@ package com.casinoreal;
 /**
  * Created by alexandraarmstrong on 1/24/17.
  */
-abstract public class CardGames {
+abstract public class CardGames extends Game{
 
     Shoe shoe;
     Card[] hand;
