b21b4a0b2454964a024474d471c89b13d8aa8aa7
Kevin McCann <kevinmccann@zipcoders-MacBook-Pro-5.local>
2017-01-24 23:32:28 -0500

    casino bar initial idea sketch

---
+++ casino/src/main/java/com/casinoreal/CasinoBar.java
@@ -0,0 +1,64 @@
+package com.casinoreal;
+
+import java.util.Random;
+
+/**
+ * Created by kevinmccann on 1/24/17.
+ */
+public class CasinoBar {
+    final String[] jokes = {
+            "A baby seal walks into a bar and sits down. \"What can I get you?\" asked the bartender. \"Anything but a Canadian Club\" replied the seal.",
+            "A guy walks into a bar carrying a pair of jumper cables. \n" +
+                    "He sets em down on the bar. \n" +
+                    "And then the bartender said \"Now dont you start anything!\"",
+            "Helium walks into a bar. \n" +
+                    "The bartender says \"We don't serve noble gasses in here.\" \n" +
+                    "Helium doesn't react.",
+            "A guy sits down at the bar and orders drink after drink rapidly.\n" +
+                    "\"Is everything okay, pal?\", the bartender asks.\n" +
+                    "\"My wife and I got into a fight and she said she isn’t talking to me for a month!\".\n" +
+                    "Trying to put a positive spin on things, the bartender says, \"Well, maybe that’s kind of a good thing. \n" +
+                    "You know, a little peace and quiet?\"\n" +
+                    "\"Yeah. But today is the last day\".",
+            "A number twelve walks into a bar and asks the barman for a pint of beer.\n" +
+                    "\"Sorry I cant serve you,\" states the barman.\n" +
+                    "\"Why not?!\" asks the number twelve with anger showing in its voice.\n" +
+                    "\"Youre under 18,\" replies the barman."
+    };
+
+//    int talkToBartender() {
+//        if(choice.equals("Tell a joke!"))
+//            bartenderJoke();
+//        if(choice.equals("Have a drink")) {
+//            updateBalance(-10);
+//            increaseDrunkMultiplier();
+//        }
+//    }
+
+    String bartenderJoke() {
+        Random random = new Random();
+        int index = random.nextInt(jokes.length);
+        return jokes[index];
+    }
+
+    //Random chance to lose money or gain a tip
+    void talkToWomanInRedDress() {
+        double chance = Math.random();
+        if (chance < .2 ) {
+            getLucky();
+        }
+        if (chance > .8) {
+//            updateBalance(-25);
+        }
+        else {
+//            System.out.println("She seems disinterested.");
+        }
+
+
+    }
+
+    void getLucky(){
+//        increaseLuckMultiplier();
+    }
+
+}
