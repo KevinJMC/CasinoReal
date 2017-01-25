b68f299054f084d0207baa2cce35ea8eeab35de8
Alexandra Armstrong <alexandraarmstrong@zipcoders-MacBook-Pro.local>
2017-01-25 09:15:33 -0500

    changes to wheel and constructor

--- casino/src/main/java/com/casinoreal/Slots.java
+++ casino/src/main/java/com/casinoreal/Slots.java
@@ -5,10 +5,51 @@ package com.casinoreal;
  */
 public class Slots {
 
-    char[] wheel = {'\u2620', '\u2615', '\u2603',
-                     '\u2618', '\u272F', '\u262F',
-                     '\u263A', '\u263B', '\u277C',
-                     '\u2757', '\u27C1', '\u2A37'};
+    final char[] wheelOptions = {'\u2618', '\u2618', '\u2618', '\u2618', '\u2618', '\u2618','\u2618', '\u2618', '\u2618',
+                                 '\u277C', '\u277C', '\u277C', '\u277C', '\u277C', '\u277C', '\u277C', '\u277C',
+                                 '\u262F', '\u262F', '\u262F', '\u262F', '\u262F', '\u262F', '\u262F',
+                                 '\u2725', '\u2725', '\u2725', '\u2725', '\u2725', '\u2725',
+                                 '\u29D7', '\u29D7', '\u29D7', '\u29D7', '\u29D7',
+                                 '\u263B', '\u263B', '\u263B', '\u263B',
+                                 '\u277C', '\u277C', '\u277C',
+                                 '\u2726', '\u2726',
+                                 '\u27C1',
+                                 '\u2A37'};
+    char[][] gameWheel = new char[3][3];
 
+    Slots() {
+        for (int i = 0; i < gameWheel.length; i++) {
+            for (int j = 0; j < gameWheel.length; j++) {
+                gameWheel[i][j] = wheelOptions[(int) (Math.random() * 12)];
+            }
+        }
+    }
+
+    protected double checkWin(){
+        return 0;
+    }
+
+    public void printWheel(){
+        for(char[] x: gameWheel){
+            System.out.println();
+            for( char y: x){
+                System.out.print(y);
+            }
+        }
+    }
+
+    public void printWheelOptions() {
+        for(char x: wheelOptions){
+            System.out.print(x);
+        }
+    }
+
+
+
+    public static void main(String [] args){
+        Slots slotGame = new Slots();
+        slotGame.printWheelOptions();
+        //slotGame.printWheel();
+    }
 }
 
