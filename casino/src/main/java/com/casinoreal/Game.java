7dbb5c713b0077d572fec6dcbf305d14920e318f
Alexandra Armstrong <alexandraarmstrong@zipcoders-MacBook-Pro.local>
2017-01-24 20:52:09 -0500

    Stubs for game class

---
+++ casino/src/main/java/com/casinoreal/Game.java
@@ -0,0 +1,21 @@
+package com.casinoreal;
+
+/**
+ * Created by alexandraarmstrong on 1/24/17.
+ */
+abstract public class Game {
+    private double bet;
+
+    //private I/O;
+
+    public void startGame(){}
+
+    abstract public void checkForWin();
+
+    public void quitGame(){}
+
+    private void cashout(){}
+
+    protected void updateBalance(){}
+
+}
