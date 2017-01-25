32030696b79be3bc460b0504ec3dd71efb7768b0
Alexandra Armstrong <alexandraarmstrong@zipcoders-MacBook-Pro.local>
2017-01-25 15:52:54 -0500

    game and cardGame and Slots edited

--- casino/src/main/java/com/casinoreal/SlotMachine.java
+++ casino/src/main/java/com/casinoreal/SlotMachine.java
@@ -3,41 +3,40 @@ package com.casinoreal;
 /**
  * Created by alexandraarmstrong on 1/25/17.
  */
-public class SlotMachine {
-    //I/O I/O;
-    //Player player;
-    double bet;
+public class SlotMachine extends Game{
 
-    Slots slots;
+    public Slots slots;
 
     public void setBet(double bet){
         if (bet == 1){
-            this.bet = 1;
+            setBet(1);
         }
         if (bet == 2){
-            this.bet = 2;
+            setBet(2);
         }
         if (bet == 3){
-            this.bet = 3;
+            setBet(3);
         }
     }
 
     public double pull(){
         slots = new Slots();
         double winnings = 0;
-        if (bet >= 1) {
-            winnings = (bet * slots.checkWinMiddle());
+        if (getBet() >= 1) {
+            winnings = (getBet() * slots.checkWinMiddle());
         }
-        if (bet >= 2){
-            winnings += (bet * slots.checkWinDiagonalLeft());
-            winnings += (bet * slots.checkWinDiagonalRight());
+        if (getBet() >= 2){
+            winnings += (getBet() * slots.checkWinDiagonalLeft());
+            winnings += (getBet() * slots.checkWinDiagonalRight());
         }
-        if (bet >= 3){
-            winnings += (bet * slots.checkWinTop());
-            winnings += (bet * slots.checkWinBottom());
+        if (getBet() >= 3){
+            winnings += (getBet() * slots.checkWinTop());
+            winnings += (getBet() * slots.checkWinBottom());
         }
         return winnings;
     }
 
+    public void startGame(){}
 
+    public void checkForWin(){}
 }
