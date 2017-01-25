9e7ee797f74333f14b4288f1190b51a5b08d2986
Alexandra Armstrong <alexandraarmstrong@zipcoders-MacBook-Pro.local>
2017-01-24 23:18:30 -0500

    wheel added

--- casino/src/main/java/com/casinoreal/Slots.java
+++ casino/src/main/java/com/casinoreal/Slots.java
@@ -5,5 +5,10 @@ package com.casinoreal;
  */
 public class Slots {
 
+    char[] wheel = {'\u2620', '\u2615', '\u2603',
+                     '\u2618', '\u272F', '\u262F',
+                     '\u263A', '\u263B', '\u277C',
+                     '\u2757', '\u27C1', '\u2A37'};
 
 }
+
