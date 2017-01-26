924d3a5e52c6a2a02d28843ca1d08ce4c3babe4f
Kevin McCann <kevinmccann@zipcoders-MacBook-Pro-5.local>
2017-01-25 22:08:07 -0500

    casinowar updated with some i/o and display

--- casino/src/main/java/com/casinoreal/CasinoWarGame.java
+++ casino/src/main/java/com/casinoreal/CasinoWarGame.java
@@ -81,7 +81,7 @@ public class CasinoWarGame extends CardGames {
             // player card greater than dealer card, aka c1 > this in the method
             if ( dealerCard.compareTo(playerCard) < 1 ) {
                 // win
-                player.setBalance(player.getBalance() += (2 * bet));
+                player.setBalance(player.getBalance() + (2 * bet));
                 IO.displayWarHand(playerCard, dealerCard, true);
                 /*System.out.println("you tied");
                 System.out.println("ask user to play again after tie logic done");*/
