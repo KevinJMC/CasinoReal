002ec505fcccfe5b6e084e28f62d7f53ef3c045a
John Squier <johnsquier@zipcoders-MacBook-Pro-3.local>
2017-01-26 11:13:21 -0500

    might have game now

---
+++ casino/src/main/java/com/casinoreal/Game.java
@@ -0,0 +1,23 @@
+package com.casinoreal;
+
+/**
+ * Created by alexandraarmstrong on 1/24/17.
+ */
+abstract public class Game {
+    protected double bet;
+    protected Player player;
+
+    abstract public void startGame();
+
+    abstract public boolean checkForWin();
+
+    public double getBet(){
+        return bet;
+    }
+
+    public void setBet(double bet){
+        this.bet = bet;
+        player.setBalance(player.getBalance() - getBet());
+    }
+
+}
