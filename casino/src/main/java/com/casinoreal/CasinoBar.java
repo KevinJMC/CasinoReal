6aceb80c3a161ca3dc5c8bf773a25ff263197f1c
John Squier <johnsquier@zipcoders-MacBook-Pro-3.local>
2017-01-26 16:37:44 -0500

    got new bar I think

--- casino/src/main/java/com/casinoreal/CasinoBar.java
+++ casino/src/main/java/com/casinoreal/CasinoBar.java
@@ -5,7 +5,9 @@ import java.util.Random;
 /**
  * Created by kevinmccann on 1/24/17.
  */
-public class CasinoBar {
+public class CasinoBar extends Game {
+
+
     final String[] jokes = {
             "A baby seal walks into a bar and sits down. \"What can I get you?\" asked the bartender. \"Anything but a Canadian Club\" replied the seal.",
             "A guy walks into a bar carrying a pair of jumper cables. \n" +
@@ -35,10 +37,11 @@ public class CasinoBar {
 //        }
 //    }
 
-    String bartenderJoke() {
+    void bartenderJoke() {
         Random random = new Random();
         int index = random.nextInt(jokes.length);
-        return jokes[index];
+        String joke = jokes[index];
+        IO.displayGenericHeaderAndMessageScreen("The bartender says, \"Lemme tell you a joke!\"", joke);
     }
 
     //Random chance to lose money or gain a tip
@@ -47,18 +50,46 @@ public class CasinoBar {
         if (chance < .2 ) {
             getLucky();
         }
-        if (chance > .8) {
-//            updateBalance(-25);
-        }
+        else if (chance > .8) {
+            IO.displayGenericHeaderAndMessageScreen("She winks at you as you walk over...", new String[]{"You buy a round of drinks",
+                    "She talks and flirts for a bit...", " ", "then leaves with her boyfriend", " ", "-$25"}) ;
+            player.setBalance(player.getBalance()-25);
+                }
+        else if (chance > .45 && chance < .52) {
+            IO.displayGenericHeaderAndMessageScreen("You strike up a conversation", new String[] {"Her eyes sparkle as she laughs at your jokes", " ", "\"Let's go for a walk,\" she says...",
+                    "You head out the back door, down a dark alley...", " ", "!!!", "She set you up, 3 men jump you, and steal your wallet!","They've taken everything!"});
+            player.setBalance(0.0);
+            }
         else {
-//            System.out.println("She seems disinterested.");
+            IO.displayGenericHeaderAndMessageScreen("You try to strike up a conversation", "She seems disinterested...");
         }
-
-
     }
 
     void getLucky(){
-//        increaseLuckMultiplier();
+        IO.displayGenericHeaderAndMessageScreen("You got lucky!", "You find $100 on the ground!");
+        player.setBalance(player.getBalance()+100);
+    }
+
+    public boolean checkForWin() { return true; }
+
+    public void startGame() {
+        boolean drinking = true;
+        while (drinking) {
+            IO.displayGenericHeaderAndMessageScreen("Welcome to the Casino Real Bar", new String[]{"What would you like to do?", " ", "1. Talk to the Bartender", " ", "2. Talk to the woman in the red dress", " ", "3. Have a ginger beer", " ", "otherwise, exit to casino"});
+            int choice = (int) IO.getInputSlotsBet();
+            switch (choice) {
+                case 1: bartenderJoke();
+                        break;
+                case 2: talkToWomanInRedDress();
+                        break;
+                case 3:
+                        IO.displayGenericHeaderAndMessageScreen(" ", "You enjoy a cool, refreshing ginger beer.");
+                        break;
+                default:
+                        drinking = false;
+            }
+        }
+        
     }
 
 }
