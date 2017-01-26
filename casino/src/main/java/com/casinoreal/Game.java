632ae9141041e16885ebead2174ac520154d9d97
Alexandra Armstrong <alexandraarmstrong@zipcoders-MacBook-Pro.local>
2017-01-26 11:26:50 -0500

    Added getter and setter to player in Game

--- casino/src/main/java/com/casinoreal/Game.java
+++ casino/src/main/java/com/casinoreal/Game.java
@@ -7,6 +7,14 @@ abstract public class Game {
     protected double bet;
     protected Player player;
 
+    public void setPlayer(Player player){
+        this.player = player;
+    }
+
+    public Player getPlayer(){
+        return player;
+    }
+
     abstract public void startGame();
 
     abstract public boolean checkForWin();
