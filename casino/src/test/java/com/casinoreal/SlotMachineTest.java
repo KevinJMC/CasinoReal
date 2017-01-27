c6bf6b4ba85dc5251dfcc79684d1141330f709d8
Alexandra Armstrong <alexandraarmstrong@zipcoders-MacBook-Pro.local>
2017-01-27 13:57:36 -0500

    tests non-functional

--- casino/src/test/java/com/casinoreal/SlotMachineTest.java
+++ casino/src/test/java/com/casinoreal/SlotMachineTest.java
@@ -10,53 +10,48 @@ import static org.junit.Assert.assertTrue;
 /**
  * Created by alexandraarmstrong on 1/25/17.
  */
-/*public class SlotMachineTest{
+public class SlotMachineTest{
     SlotMachine slotMachine;
-    Player player;
+    //Player player;
 
     @Before
     public void setUp(){
+        //player = new Player();
+        //player.setName("alex");
+        //player.setBalance(10000d);
         slotMachine = new SlotMachine();
-        player = new Player();
-        player.setBalance(10000d);
     }
-    @Ignore
+
     @Test
     public void setPlayerWagerTest(){
         slotMachine.setBet(2.0);
         assertEquals(2d, slotMachine.getBet());
     }
-    @Ignore
     @Test
     public void setPlayerWagerTest1(){
         slotMachine.setBet(1.0);
         assertEquals(1d, slotMachine.getBet());
     }
-    @Ignore
     @Test
     public void setPlayerWagerTest2(){
         slotMachine.setBet(3.0);
         assertEquals(3d, slotMachine.getBet());
     }
-    @Ignore
     @Test
     public void pullTest(){
         slotMachine.setBet(1.0);
         assertTrue(((slotMachine.pull() >= 0d) && (slotMachine.pull() <= 400d)));
     }
-    @Ignore
     @Test
     public void pullTest1(){
         slotMachine.setBet(2.0);
         assertTrue(((slotMachine.pull() >= 0d) && (slotMachine.pull() <= 800d)));
     }
-    @Ignore
     @Test
     public void pullTest2(){
         slotMachine.setBet(3.0);
         assertTrue(((slotMachine.pull() >= 0d) && (slotMachine.pull() <= 1200d)));
     }
-    @Ignore
     @Test
     public void checkForWinTest(){
         slotMachine.setBet(1);
@@ -64,4 +59,4 @@ import static org.junit.Assert.assertTrue;
         boolean expected = true || false;
         assertEquals(expected, actual);
     }
-}*/
+}
